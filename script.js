ce7bb496ec551b82b639bbfe3526763805ffe570
shway250 <jackson_duhon@yahoo.com>
2016-09-05 18:01:06 -0700

    got sketch in background, working on postitioning elements of top

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
-var snow;  // Declare object
+var snow = [];
+var number_of_flakes = 2000;
 
 function setup() {
-  createCanvas(710, 400);
+  var canvas = createCanvas(displayWidth, displayHeight);
+  canvas.parent("myContainer");
   // Create object
-  snow = new Jitter();
+  for (var i = 0; i < number_of_flakes; i++){
+    snow[i] = new Jitter();
+  }
 }
 
 function draw() {
   background(255);
-  snow.move();
-  snow.display();
+  for (var i = 0; i < number_of_flakes; i++){
+    snow[i].move();
+    snow[i].display();
+    snow[i].bringUp();
+  }
 }
-
-// // Jitter class
-// function Jitter() {
-//   this.x = random(width);
-//   this.y = random(height);
-//   this.diameter = random(10, 30);
-//   this.speed = 1;
-
-
-//   this.move = function() {
-//     this.x += random(-this.speed, this.speed);
-//     this.y += random(-this.speed, this.speed);
-//   };
-
-//   this.display = function() {
-//     ellipse(this.x, this.y, this.diameter, this.diameter);
-//   }
-// };
 
 // Jitter class
 function Jitter() {
   this.x = random(width);
   this.y = random(height);
-  this.diameter = random(10, 30);
   this.speed = 1;
   //rgb values
   this.r = random(0,100);
@@ -43,15 +31,28 @@ function Jitter() {
 
   this.move = function() {
     this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed* random(4, 25));
   };
 
   this.display = function() {
     stroke(255);
     strokeWeight(0);
+    //making Shape
+    rect(this.x,this.y,3,30);
+  }
+
+  this.bringUp = function() {
     fill(this.r,this.g,this.b);
-    //making shape
-    ellipse(this.x, this.y, this.diameter, this.diameter);
-    rect(20,30,2,20);
+    //X Axis
+    if (this.x > width){
+    this.x=0;
+    }
+    else if (this.x < 0){
+    this.x=width;
+    }
+    //Y Axis
+    if (this.y > height){
+    this.y = 0;
+    }
   }
 };
